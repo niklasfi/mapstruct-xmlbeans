0952d48bafd533aff67a7ff618088ef4569ec8b8
Niklas Fischer <niklas.fischer@pwc.com>
2022-05-17 17:25:44 +0200

    [broken] use lombok in Car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,27 +1,13 @@
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Car {
 
     private String make;
 
     private int numberOfSeats;
-
-    //constructor, getters, setters etc.
-
-    public Car() {
-    }
-
-    public String getMake() {
-        return make;
-    }
-
-    public void setMake(String make) {
-        this.make = make;
-    }
-
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
 }
