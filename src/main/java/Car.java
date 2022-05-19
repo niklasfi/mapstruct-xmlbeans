385a5b72d8c0dc023554df392ed453d5ee5d2f45
Niklas Fischer <niklas.fischer@pwc.com>
2022-05-19 07:19:38 +0200

    remove lombok. Error still occurs

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,13 +1,26 @@
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@Getter
-@Setter
 public class Car {
 
     private String make;
 
     private int numberOfSeats;
+
+    public Car() {
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
 }
